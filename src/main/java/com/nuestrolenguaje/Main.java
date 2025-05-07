5be85a274d9a2f73adcd39b0f1c3201f9997def9
PaulinaBV <paulinabelen@gmail.com>
2025-05-07 12:22:12 -0600

    Se quitaron los comentarios que salian en consola

--- src/main/java/com/nuestrolenguaje/Main.java
+++ src/main/java/com/nuestrolenguaje/Main.java
@@ -4,21 +4,25 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
-import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
 
 public class Main {
 
     private static final String EXTENSION = "manbel";
     private static final String DIRBASE = "src/test/resources/";
+    
 
     public static void main(String[] args) throws IOException {
         System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
         System.setErr(new PrintStream(System.err, true, StandardCharsets.UTF_8));
         String files[] = args.length == 0 ? new String[] { "test." + EXTENSION } : args;
-        System.out.println("Dirbase: " + DIRBASE);
+
+        
 
         for (String file : files) {
-            System.out.println("START: " + file);
 
             CharStream in = CharStreams.fromFileName(DIRBASE + file);
             manbelLexer lexer = new manbelLexer(in);
@@ -26,30 +30,22 @@ public class Main {
             manbelParser parser = new manbelParser(tokens);
 
             // Manejador de errores personalizado
-            parser.removeErrorListeners();
+            /*parser.removeErrorListeners();
             parser.addErrorListener(new DiagnosticErrorListener()); // Para más detalles
-            parser.addErrorListener(new ConsoleErrorListener()); // Para imprimir en consola
+            parser.addErrorListener(new ConsoleErrorListener()); */ //Para imprimir en consola
 
             // Intenta obtener el árbol
             try {
                 manbelParser.ProgramaContext tree = parser.programa();
-                System.out.println("Arbol generado:\n" + tree.toStringTree(parser));
                 tokens.fill();
-                for (Token token : tokens.getTokens()) {
-                    if (token.getType() != Token.EOF) {
-                        System.out.println(lexer.getVocabulary().getSymbolicName(token.getType()) +
-                                ": '" + token.getText() + "'");
-                    }
-                }
 
                 manbelCustomVisitor visitor = new manbelCustomVisitor();
                 visitor.visit(tree);
             } catch (RecognitionException | IllegalArgumentException e) {
                 System.err.println("Error durante la ejecución:");
-                System.err.println("An error occurred: " + e.getMessage());
+                System.err.println("Esto salio mal carnal: " + e.getMessage());
             }
 
-            System.out.println("FINISH: " + file);
         }
     }
 
